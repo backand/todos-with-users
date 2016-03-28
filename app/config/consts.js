599057b30f2b6b6d7b889c63865164a3a1fc2075
yariv@backand.com <yariv@backand.com>
2016-03-28 23:18:40 +0300

    twitter

--- app/config/consts.js
+++ app/config/consts.js
@@ -1,37 +1,9 @@
-/*(function() {
+(function() {
   angular.module('mytodoApp.config.consts', [])
     .constant('CONSTS', {
-      anonymousToken: 'd6fde620-99ad-43c1-8ef9-acf73b06453b',
-      signUpToken: 'cc92be03-15c0-41db-9036-be5517af3c5f',
-      appName: 'djtodowithusers'
+      anonymousToken: '1effc00d-3f9e-4dde-8b8c-a49d43728ea6',
+      signUpToken: 'cbb66bf1-36ab-4ead-8140-022c13e01b61',
+      appName: 'mytododemo'
     });
-
-})();
-(function() {
-    angular.module('mytodoApp.config.consts', [])
-        .constant('CONSTS', {
-            anonymousToken: '27dadd4e-09d0-40aa-8740-0029593472f4',
-            signUpToken: 'bbf50e63-3a75-4505-ba70-e607b19bdadb',
-            appName: 'todo219615'
-        });
-
-})();
-
-(function() {
-    angular.module('mytodoApp.config.consts', [])
-        .constant('CONSTS', {
-            anonymousToken: 'ddafae17-efe5-47d4-9ebc-a1ef199a7e9e',
-            signUpToken: 'e05e4fb6-cf20-417a-b249-a3433d057e93',
-            appName: 'yrvfbtest'
-        });
-
-})();*/
-(function() {
-    angular.module('mytodoApp.config.consts', [])
-        .constant('CONSTS', {
-            anonymousToken: '89091f5e-17b5-4db2-86e2-a33a492eb045',
-            signUpToken: '4d15f38f-9e9c-43a6-9465-ba90fe3dd211',
-            appName: 'y201603280200'
-        });
 
 })();
