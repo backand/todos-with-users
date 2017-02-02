10e3b4fc409417318c4d4fdc36a2bcf7d393f6e8
Matt Billock <mattbillock@mattbillock.com>
2017-02-02 08:04:15 -0600

    Fetching data properly now, but there is still an error

--- app/config/consts.js
+++ app/config/consts.js
@@ -1,9 +1,9 @@
 (function() {
   angular.module('mytodoApp.config.consts', [])
     .constant('CONSTS', {
-      anonymousToken: '1effc00d-3f9e-4dde-8b8c-a49d43728ea6',
-      signUpToken: 'cbb66bf1-36ab-4ead-8140-022c13e01b61',
-      appName: 'mytododemo'
+      anonymousToken: 'cadf93db-db2a-441d-be5d-886d40eb9095',
+      signUpToken: '57061b78-9ff8-4188-97fc-d1609d25c9ea',
+      appName: 'billocktodowithusers2'
     });
 
 })();
